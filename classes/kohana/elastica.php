fd9a4f4e2a49604335648e200780e0fba2cb7bca
Guillaume Couture <coutureguillaume@gmail.com>
2014-02-05 20:09:58 -0500

    Update elastica.php
    Make sure autoload uses namespaces properly

--- classes/kohana/elastica.php
+++ classes/kohana/elastica.php
@@ -2,47 +2,43 @@
 
 class Kohana_Elastica
 {
-	
 	protected static $vendor_class_root; 
 	
 	/**
 	 * Registers autoloader method for Elastica classes
-	 * 
-	 * @param string $module_path
-	 * 
+	 * @param  string $module_path
 	 * @return void
 	 */
 	public static function register_autoloader($module_path)
 	{
-		// get path to Elastica's files
+		//Get path to Elastica's files
 		self::$vendor_class_root = $module_path.'/vendor/Elastica/lib';
 		
-		// check that path exists
+		//Check that path exists
 		if (!file_exists(self::$vendor_class_root))
 			throw new Exception(sprintf("Elastica path %s not found", self::$vendor_class_root));
 		
-		// register autoloader
+		//Register autoloader
 		spl_autoload_register('Kohana_Elastica::autoload');		
 	}
 
-
 	/**
 	 * Autoloader for Elastica classes
-	 * @param string $class
+	 * @param  string The classname
 	 * @return void
 	 */
 	public static function autoload($class)
 	{
-		$filename = str_replace("\\", '/', $class) . '.php';
-		$file = self::$vendor_class_root . '/' . $filename;
-
-		require_once $file;
+		//Make sure this is an elastica class
+ 		if (substr($class, 0, 9) == 'Elastica\\')
+			require_once self::$vendor_class_root . '/' . str_replace("\\", '/', $class) . '.php';
 	}
 	
 	
 	/**
-	 * @param string $config
-	 * @return Elastica_Config
+	 * Get Elastica client
+	 * @param  string Config name
+	 * @return object
 	 */
 	public static function get_client($config_name = 'default')
 	{
