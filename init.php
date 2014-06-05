82e025bd80e1482cd3356fc2d2e7d4c63a112bbd
Guillaume Couture <coutureguillaume@gmail.com>
2014-06-04 20:49:25 -0400

    Remove useless code in inti.php and PSR-0 on classes

--- init.php
+++ init.php
@@ -1,10 +1,4 @@
 <?php
-
-
-// get path to module
-$module_path = realpath(dirname(__FILE__));
-
-
 // register autoloader with module path
 Kohana_Elastica::register_autoloader(
 	realpath(dirname(__FILE__))
